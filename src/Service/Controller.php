54a5634b13c85dbf1207b97bab224ceb1db15e04
zodream <648383079@qq.com>
2018-12-06 16:08:06 +0800

    修改

--- src/Service/Controller.php
+++ src/Service/Controller.php
@@ -1,7 +1,7 @@
 <?php
 namespace Zodream\Debugger\Service;
 
-use Zodream\Service\Controller\ModuleController as BaseController;
+use Zodream\Route\Controller\ModuleController as BaseController;
 
 abstract class Controller extends BaseController {
 
