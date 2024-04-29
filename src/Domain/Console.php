40707663df3b87ae2648f2027ff80899e3e73ea9
ZoDream <648383079@qq.com>
2024-04-29 16:15:16 +0800

    update console log

--- src/Domain/Console.php
+++ src/Domain/Console.php
@@ -3,10 +3,9 @@ declare(strict_types=1);
 namespace Zodream\Debugger\Domain;
 
 
-class Log {
+class Console {
 
     const COLOR_DEFAULT = "\e[0m";
-
     const COLOR_BLACK = "\e[30m\e[1m";
     const COLOR_ORANGE = "\e[38;5;208m";
     const COLOR_BLUE = "\e[34m";
@@ -18,19 +17,19 @@ class Log {
 
     const COLOR_WHITE = "\e[97m";
 
-    public static function error(mixed $message) {
+    public static function error(mixed $message): void {
         static::info($message, self::COLOR_RED);
     }
 
-    public static function warning(mixed $message) {
+    public static function warning(mixed $message): void {
         static::info($message, self::COLOR_ORANGE);
     }
 
-    public static function notice(mixed $message) {
+    public static function notice(mixed $message): void {
         static::info($message, self::COLOR_WHITE);
     }
 
-    public static function info(mixed $message, ?string $color = null) {
+    public static function info(mixed $message, ?string $color = null): void {
         if (!app('request')->isCli()) {
             return;
         }
@@ -41,8 +40,12 @@ class Log {
         echo $color,$message,self::COLOR_DEFAULT,PHP_EOL;
     }
 
-    public static function removeLine() {
-        system("tput cuu1");
-        system("tput el");
+    public static function removeLine(): void {
+        if (!app('request')->isCli()) {
+            return;
+        }
+        // TODO cmd powersehll 不支持
+        system('tput cuu1');
+        system('tput el');
     }
 }
